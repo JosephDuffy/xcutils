1a35d9eeb9fd61b4b2799f1d7deaf61328b9f2e5
Joseph Duffy <commits@josephduffy.co.uk>
2020-01-28 09:04:56 -0800

    Add support for running tests on macOS

--- Sources/TestRunner/TestRunner.swift
+++ Sources/TestRunner/TestRunner.swift
@@ -31,63 +31,65 @@ public final class TestRunner {
     }
     
     public static func runTests(platform: Platform, scheme: String, project: String? = nil) throws {
-        let decoder = JSONDecoder()
-
-        let runtimesData = try run("xcrun", "simctl", "list", "runtimes", "--json")
-        let decoded = try decoder.decode(RuntimesOutput.self, from: runtimesData)
-        let runtimes = decoded.runtimes
-
-        let supportedRuntimes = runtimes.filter { $0.name.lowercased().contains(platform.name.lowercased()) }
-
-        guard !supportedRuntimes.isEmpty else {
-            printError("Found no runtimes for platform: \(platform)")
-            exit(1)
-        }
+        let destination: String
         
-        let runtime: Runtime
+        switch platform.name.lowercased() {
+        case "macos":
+            destination = "platform=macOS"
+        default:
+            let decoder = JSONDecoder()
 
-        switch platform.version {
-        case .latest:
-            runtime = supportedRuntimes.sorted(by: { $0.version > $1.version }).first!
-        case .specific(let version):
-            if let foundVersion = supportedRuntimes.first(where: { $0.version == version }) {
-                runtime = foundVersion
-            } else {
-                printError("Failed to find runtime with version", version)
+            let runtimesData = try run("xcrun", "simctl", "list", "runtimes", "--json")
+            let decoded = try decoder.decode(RuntimesOutput.self, from: runtimesData)
+            let runtimes = decoded.runtimes
+
+            let supportedRuntimes = runtimes.filter { $0.name.lowercased().contains(platform.name.lowercased()) }
+
+            guard !supportedRuntimes.isEmpty else {
+                printError("Found no runtimes for platform: \(platform)")
                 exit(1)
             }
+            
+            let runtime: Runtime
+
+            switch platform.version {
+            case .latest:
+                runtime = supportedRuntimes.sorted(by: { $0.version > $1.version }).first!
+            case .specific(let version):
+                if let foundVersion = supportedRuntimes.first(where: { $0.version == version }) {
+                    runtime = foundVersion
+                } else {
+                    printError("Failed to find runtime with version", version)
+                    exit(1)
+                }
+            }
+            
+            let devicesData = try run("xcrun", "simctl", "list", "devices", "--json")
+            let devices = try decoder.decode(DevicesOutput.self, from: devicesData)
+            let simulators = devices.devices
+
+            guard let supportedSimulators = simulators[runtime.identifier] else {
+                printError("Found no simulators for platform", platform)
+                exit(1)
+            }
+
+            let availableSimulators = supportedSimulators.filter { $0.isAvailable }
+
+            guard !availableSimulators.isEmpty else {
+                printError("All simulators for platform \(platform) are unavailable")
+                exit(1)
+            }
+
+            let simulator = availableSimulators.first(where: { $0.state == .booted }) ?? availableSimulators.first!
+            destination = "id=\(simulator.udid.uuidString)"
         }
 
-        print("Looking for a simulators with runtime identifier", runtime.identifier)
-        
-        let devicesData = try run("xcrun", "simctl", "list", "devices", "--json")
-        let devices = try decoder.decode(DevicesOutput.self, from: devicesData)
-        let simulators = devices.devices
-
-        guard let supportedSimulators = simulators[runtime.identifier] else {
-            printError("Found no simulators for platform", platform)
-            exit(1)
-        }
-
-        let availableSimulators = supportedSimulators.filter { $0.isAvailable }
-
-        guard !availableSimulators.isEmpty else {
-            printError("All simulators for platform \(platform) are unavailable")
-            exit(1)
-        }
-
-        print("Picking from", availableSimulators)
-
-        let simulator = availableSimulators.first(where: { $0.state == .booted }) ?? availableSimulators.first!
-
-        print("Using simulator", simulator)
-        
         var command: [String] = [
             "xcodebuild",
             "-scheme",
             scheme,
             "-destination",
-            "id=\(simulator.udid.uuidString)",
+            destination,
         ]
         
         if let project = project {
