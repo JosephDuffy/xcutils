0351c9bb8518df223221b608bafea182a2587a8f
Joseph Duffy <commits@josephduffy.co.uk>
2020-07-19 22:14:42 +0100

    Use build to determine Xcode version order

--- Sources/XcodeSelect/XcodeVersion.swift
+++ Sources/XcodeSelect/XcodeVersion.swift
@@ -5,7 +5,77 @@ public struct XcodeVersion: Comparable, CustomStringConvertible, Codable {
     
     public static func < (lhs: XcodeVersion, rhs: XcodeVersion) -> Bool {
         if lhs.version == rhs.version {
-            return lhs.isBeta && !rhs.isBeta || lhs.bundleVersion < rhs.bundleVersion
+            if lhs.isBeta && !rhs.isBeta {
+                return true
+            }
+
+            // The build is usually in the format {major-version}{single-letter}{number}, sometimes
+            // with another single character on the end. This is a crude way to attempt to parse
+            // this and will need to be tested with more versions.
+            let majorVersionString = "\(lhs.version.major)"
+            if lhs.build.starts(with: majorVersionString) && rhs.build.starts(with: majorVersionString) {
+                var lhsBuild = lhs.build
+                var rhsBuild = rhs.build
+                lhsBuild = String(lhsBuild.dropFirst(majorVersionString.count))
+                rhsBuild = String(rhsBuild.dropFirst(majorVersionString.count))
+
+                enum SearchMode {
+                    case letters
+                    case numbers
+                }
+
+                var searchMode: SearchMode = .letters
+
+                outerWhile: while !lhsBuild.isEmpty && !rhsBuild.isEmpty {
+                    let predicate = { (character: Character) -> Bool in
+                        switch searchMode {
+                        case .letters:
+                            return character.isLetter
+                        case .numbers:
+                            return character.isNumber
+                        }
+                    }
+
+                    let lhsCharacters = lhsBuild.prefix(while: predicate)
+                    let rhsCharacters = rhsBuild.prefix(while: predicate)
+
+                    defer {
+                        lhsBuild = String(lhsBuild.dropFirst(lhsCharacters.count))
+                        rhsBuild = String(rhsBuild.dropFirst(rhsCharacters.count))
+                    }
+
+                    switch searchMode {
+                    case .letters:
+                        defer {
+                            searchMode = .numbers
+                        }
+
+                        guard lhsCharacters.count == rhsCharacters.count else { break outerWhile }
+
+                        for (index, lhsCharacter) in lhsCharacters.enumerated() {
+                            let rhsCharacter = rhsCharacters[rhsCharacters.index(rhsCharacters.startIndex, offsetBy: index)]
+
+                            if rhsCharacter == lhsCharacter { continue }
+
+                            return lhsCharacter < rhsCharacter
+                        }
+                    case .numbers:
+                        defer {
+                            searchMode = .letters
+                        }
+
+                        guard let lhsNumber = Int(lhsCharacters), let rhsNumber = Int(rhsCharacters) else { break outerWhile }
+
+                        if lhsNumber == rhsNumber { continue }
+
+                        return lhsNumber < rhsNumber
+                    }
+                }
+            }
+
+            // This is not always accurate (e.g. Xcode 12 beta 1 vs Xcode 12 beta 2), but hopefully
+            // this is never reached.
+            return lhs.bundleVersion < rhs.bundleVersion
         } else {
             return lhs.version < rhs.version
         }
