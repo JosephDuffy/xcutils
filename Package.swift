33411b82937c31782c3c46d2de886d4f9d6b2863
Joseph Duffy <commits@josephduffy.co.uk>
2020-08-16 20:11:07 +0100

    Add .git to swift-argument-parser package URL

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/mxcl/Version.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.2.2"),
     ],
     targets: [
         .target(name: "xcutils", dependencies: ["XcutilsCommand"]),
